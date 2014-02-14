2b3b94f08ae8a4562600dbcd0a2bdcc7ac9d1b41
defor666 <defor@seznam.cz>
2014-02-14 12:31:48 +0100

    * Actor removed from Classes

--- Engine/Plugins/TrueSkyPlugin/Source/TrueSkyPlugin/Classes/TrueSkySequenceActor.cpp
+++
@@ -1,9 +0,0 @@
-#include "TrueSkyPluginPrivatePCH.h"
-#include "TrueSkySequenceActor.h"
-
-ATrueSkySequenceActor::ATrueSkySequenceActor(const class FPostConstructInitializeProperties& PCIP)
-	: Super(PCIP)
-{
-
-}
-
